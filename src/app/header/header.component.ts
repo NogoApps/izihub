d4536d2099444beaff981012b23fc141800d2cd4
GorickUPM <danilosilvaweb@hotmail.com>
2017-01-01 12:03:17 -0300

    issue #01 area hidden doesn't work

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NewTutorialComponent } from '../new-tutorial/new-tutorial.component';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+  addTutorial(){
+    //new NewTutorialComponent();
+  }
 
   ngOnInit() {
   }
